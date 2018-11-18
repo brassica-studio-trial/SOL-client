5aeeed76dbb3fbfe8b35f862fb0c96056219b12b
naoyuki-murai-bs <naoyuki.murai.bs@gmail.com>
2018-11-19 00:35:02 +0900

    新規作成

---
+++ Assets/Scripts/TitleSceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleSceneController : MonoBehaviour
+{
+
+    // Inspector上で次のシーン名を設定
+    public string nextSceneName;
+
+    void changeNext()
+    {
+        if (Time.timeSinceLevelLoad > 3.0f)
+        {
+            nextSceneName = "Main";
+            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeNext();
+    }
+
+}
